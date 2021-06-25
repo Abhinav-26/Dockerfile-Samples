551ae82ca00802081bc5544f9ab7e91d6cd9ff42
Abhinav-26 <abhinavdubey616@gmail.com>
2021-06-25 19:58:32 +0530

    index.php minor chnages

--- php-app/index.php
+++ php-app/index.php
@@ -5,11 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
-    <title>NodeApp</title>
+    <title>PhpApp</title>
 </head>
 <body>
     <div class="typewriter">
-        <h1>Hello World! Welcome to the Node Js Application!</h1>
+        <h1>Hello World! Welcome to the PHP Application!</h1>
     </div>
     
     <img src="images/hello_world.png" alt="Hello World">
